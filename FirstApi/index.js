a8990fafce65432fdc607e79c5cedb34507a060a
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2025-01-13 21:50:39 +0530

    middleware

--- FirstApi/index.js
+++ FirstApi/index.js
@@ -1,12 +1,24 @@
-const express = require("express"); //ES5
-
+// const express = require("express"); //ES5
+import express from 'express';
 const app = express();
 const port = 5001;
 
-app.get("/login", (req, res) => {
-  console.log("/login api is working");
-  return res.send("/login GET api is working");
+// Adds middleware to the application’s request-processing pipeline.
+// Middleware is a function that is ALWAYS called before any route is handled.
+// Logging Middleware
+app.use((req, res, next) => {
+  const time = new Date().toTimeString();
+  console.log("Time: ", time, "Method:", req.method, ",Path:\"", req.path, "\"");
+  next();
+})
+
+app.get("/", (req, res) => {
+  return res.send("Hello World");
 });
+
+app.get("/login", (req, res) => {
+  return res.send("This is a login page");
+})
 
 app.listen(port, () => {
   console.log(`server is running on PORT : ${port}`);
