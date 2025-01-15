427d4a605397f8797dafd043c5c638c08ca9841e
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2025-01-15 17:09:24 +0530

    Dynamic Routing

--- FirstApi/index.js
+++ FirstApi/index.js
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const port = 5001;
 
+app.use(express.json())
+
+//middleware for urlencoded data
+// app.use(urlencoded({extended:true}));
+
 // Adds middleware to the application’s request-processing pipeline.
 // Middleware is a function that is ALWAYS called before any route is handled.
 // Logging Middleware
@@ -18,6 +23,23 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   return res.send("This is a login page");
+})
+
+// Dynamic Routing
+app.get('/roadmaps/:id', (req, res) => {
+  const id = req.params.id;
+  return res.send(`Hello ${id}`);
+})
+
+// Query Parameter
+app.get("/watch", (req, res)=>{
+  const v = req.query.v;
+  res.send(`Watching video ${v}`);
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send("Data Recieved");
 })
 
 app.listen(port, () => {
