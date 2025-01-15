819a83ae062a1c0bd9d1ed9762ba6e76cfd03c5e
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2025-01-15 19:34:16 +0530

    remove

--- MvcArchitecture/middleware/logMiddleware.js
+++
@@ -1,8 +0,0 @@
-
-const logMV = (req, res, next) => {
-    const time = new Date().toTimeString();
-    console.log("Time: ", time, "Method:", req.method, ",Path:\"", req.path, "\"");
-    next();
-};
-
-export default logMV;
